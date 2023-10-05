41defecf51a06b2186a4cd86d81da92a4d2def3c
mjmccain <58721408+mjmccain@users.noreply.github.com>
2023-10-05 11:51:35 -0600

    Update excel_to_ansbile_var.py

--- excel_to_ansbile_var.py
+++ excel_to_ansbile_var.py
@@ -4,6 +4,7 @@ import pandas as pd
 print("Enter the path of the Excel file to read.")
 excel_file_path = input()  # Replace with the path to your Excel file
 df = pd.read_excel(excel_file_path, engine='openpyxl')
+print(df)
 
 #input the excel column headers
 print("Enter the Excel hostname column text.")
@@ -11,24 +12,42 @@ hostname_column = input()
 print('Enter the Excel Ip Address column text.')
 ip_column = input()
 
-
 # Create an empty list to store the DNS records
 dns_records = []
 
+print("Are you building management ip[y,n]?")
+man_ip = input()
 # Loop through the rows of the dataframe and create dns records
 for index, row in df.iterrows():
-    hostname = row[hostname_column]
-    ipaddress = row[ip_column]
-    dns_record = {'host': hostname, 'ipadd': ipaddress}
+    if man_ip == 'y':
+        hostname = row[hostname_column] + 'm'
+    else:
+        hostname = row[hostname_column]
+    if '.0' in str(row[ip_column]):
+        ipaddress = row[ip_column].split('.0')
+        ipaddress = ipaddress[0] + '.' + ipaddress[1]
+    else:
+        ipaddress = row[ip_column]
+    if 'lsrv' in hostname or 'lwks' in hostname:
+        domain = 'lmfs.com'
+    elif 'wsrv' in hostname or 'wwks' in hostname:
+        domain = 'win.lmfs.com'
+    else:
+        domain = 'lmfs.com'
+    dns_record = {'host': hostname, 'ipadd': ipaddress, 'domain': domain}
     dns_records.append(dns_record)
 
 # Create the Ansible variables file
 header = '---\ndns:\n'
 for dns_record in dns_records:
-    header += f'  - host: {dns_record["host"]}\n\tipadd: {dns_record["ipadd"]}\n'
+    header += f'  - host: {dns_record["host"]}\n    ipadd: {dns_record["ipadd"]}\n    domain: {dns_record["domain"]}\n'
 
 # Write the Ansible variables file to a file
-ansible_vars_file = 'dns_vars.yml'
+if man_ip == 'y':
+    ansible_vars_file = 'dns_vars_oobm.yml'
+else:
+    ansible_vars_file = 'dns_vars_ops.yml'
+
 with open(ansible_vars_file, 'w') as file:
     file.write(header)
 
